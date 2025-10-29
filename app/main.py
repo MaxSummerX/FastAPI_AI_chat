91492c2738309fc79075b5994d96ec4dd86b60e1
MaxSummerX <marspowerpops@gmail.com>
2025-10-29 14:16:13 +0300

    adden new endpoints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from app.api.v1 import users
+from app.api.v1 import convertion, users
+from app.utils import polygon
 
 
 # Создаём приложение FastAPI
@@ -9,3 +10,7 @@ app = FastAPI(title="AI chat", version="0.1.0")
 
 # Подключаем маршруты категорий
 app.include_router(users.router_v1)
+app.include_router(convertion.router_v1)
+
+# тестовые маршруты
+app.include_router(polygon.polygon_func)
