ddb9ed535076159e6dfe463a72fe29a507e9c3f7
MaxSummerX <marspowerpops@gmail.com>
2025-10-31 20:23:07 +0300

    Added CORS for test pages and connected new endpoints for conversations

--- app/main.py
+++ app/main.py
@@ -1,16 +1,32 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api.v1 import convertion, users
-from app.utils import polygon
+from app.api.v2 import convertion_v2
 
+
+# Статические файлы
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
 
 # Создаём приложение FastAPI
-app = FastAPI(title="AI chat", version="0.1.0")
+app = FastAPI(title="AI chat", version="0.1.1")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # В продакшене укажи конкретные домены
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Подключаем маршруты категорий
 app.include_router(users.router_v1)
 app.include_router(convertion.router_v1)
+app.include_router(convertion_v2.router_v2)
 
-# тестовые маршруты
-app.include_router(polygon.polygon_func)
+# Монтируем статики
+app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="static")
