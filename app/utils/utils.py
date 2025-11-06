0f18978712687668234f62caed56a875e7feaf76
MaxSummerX <marspowerpops@gmail.com>
2025-11-06 21:44:58 +0300

    Added a function for preparing facts for inclusion in the prompt. A system prompt has been added to the basic function for transferring history. A new history function has been created for adding facts based on the relevance of the request. The function for saving messages from llm after a streaming response has been redesigned.

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,10 +1,14 @@
 import re
+from collections.abc import Awaitable
 from uuid import UUID
 
 from sqlalchemy import select
 
 from app.database.postgres_db import AsyncSession
+from app.depends.mem0_depends import get_memory
 from app.models.messages import Message as MessageModel
+from app.prompts.prompts_base import BASE_PROMPT
+from app.schemas.messages import HistoryMessage
 
 
 def extract_json(text: str) -> str:
@@ -22,6 +26,16 @@ def extract_json(text: str) -> str:
     return json_str
 
 
+def parse_facts_from_mem0(memory: dict) -> str:
+    data = []
+    for item in memory["results"]:
+        if item["score"] >= 0.75:
+            data.append(" -" + item["memory"] + " - " + item["created_at"][:16])
+
+    new_data = "\n".join(data)
+    return new_data
+
+
 async def get_conversation_history(db: AsyncSession, conversation_id: UUID, limit: int = 10) -> list[dict]:
     """Получить историю в формате для LLM"""
 
@@ -34,12 +48,57 @@ async def get_conversation_history(db: AsyncSession, conversation_id: UUID, limi
 
     messages = result.all()
 
+    # Нормализуем через Pydantic
+    history = [HistoryMessage.model_validate(msg).model_dump(mode="json") for msg in reversed(messages)]
+
     # Преобразуем в формат для LLM
-    return [{"role": msg.role, "content": msg.content} for msg in reversed(messages)]
+    return [{"role": "system", "content": BASE_PROMPT}] + history
+
+
+async def get_conversation_history_with_mem0(
+    message: str, user_name: str, prompt: str, db: AsyncSession, conversation_id: UUID, limit: int = 10
+) -> list[dict]:
+    """Получить историю в формате для LLM"""
+
+    result = await db.scalars(
+        select(MessageModel)
+        .where(MessageModel.conversation_id == conversation_id)
+        .order_by(MessageModel.timestamp.desc())
+        .limit(limit)
+    )
+
+    messages = result.all()
+
+    async with get_memory() as memory:
+        memory = await memory.search(message, user_id=str(user_name), limit=10)
+
+    new_prompt = prompt + parse_facts_from_mem0(memory)
+
+    # Нормализуем через Pydantic
+    history = [HistoryMessage.model_validate(msg).model_dump(mode="json") for msg in reversed(messages)]
+
+    # Преобразуем в формат для LLM
+    return [{"role": "system", "content": new_prompt}] + history
 
 
 async def save_message_to_db(db: AsyncSession, conversation_id: UUID, content: str, model: str) -> None:
     """Сохранение сообщения от llm в БД"""
     assistant_message = MessageModel(conversation_id=conversation_id, role="assistant", content=content, model=model)
     db.add(assistant_message)
+    await db.commit()
+
+
+async def save_message_to_db_after_stream(
+    result: Awaitable[str], db: AsyncSession, conversation_id: UUID, model: str
+) -> None:
+    """Сохранение сообщения от llm в БД после стримингого ответа"""
+
+    full_response = await result
+
+    assistant_message = MessageModel(
+        conversation_id=conversation_id, role="assistant", content=full_response, model=model
+    )
+
+    db.add(assistant_message)
+
     await db.commit()
