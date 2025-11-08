1fbc51882c8abe1c6296a49c92ddf96c3299213e
MaxSummerX <marspowerpops@gmail.com>
2025-11-08 23:09:25 +0300

    Minor logging has been added. An endpoint that can stream model messages has been implemented. Functionality for the mem0ai SDK has also been added.

--- app/api/v1/convertion.py
+++ app/api/v1/convertion.py
@@ -1,54 +1,50 @@
-import os
 from typing import cast
 from uuid import UUID
 
-from dotenv import load_dotenv
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
+from fastapi.responses import StreamingResponse
+from loguru import logger
 from mem0 import AsyncMemory
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth.dependencies import get_current_user
-from app.configs.llms.openai import OpenAIConfig
+from app.configs.llm_config import base_config_for_llm
 from app.configs.memory import custom_config
 from app.depends.db_depends import get_async_postgres_db
 from app.llms.openai import AsyncOpenAILLM
 from app.models import Conversation as ConversationModel
 from app.models import Message as MessageModel
 from app.models import User as UserModel
+from app.prompts.prompts_base import START_PROMPT
 from app.schemas.coversations import ConversationCreate
 from app.schemas.coversations import ConversationResponse as ConversationSchemas
 from app.schemas.messages import MessageCreate
 from app.schemas.messages import MessageResponse as MessageSchemas
-from app.utils.utils import get_conversation_history
+from app.utils.utils import (
+    get_conversation_history,
+    get_conversation_history_with_mem0,
+    save_message_to_db_after_stream,
+)
 
+
+router_v1 = APIRouter(prefix="/conversations", tags=["Conversations"])
 
 memory_local = AsyncMemory(config=custom_config)
 
-router_v1 = APIRouter(prefix="/conversations", tags=["conversations"])
-
-load_dotenv()
-
-
-config = OpenAIConfig(
-    model=os.getenv("MODEL"),
-    temperature=0.6,
-    api_key=os.getenv("OPENROUTER_API_KEY"),
-    max_tokens=1000,
-)  # TODO: Вынести формирование config подключения к llm в отдельную структуру чтобы пользователь мог менять настройки и прописывать свои API ключи
-
-llm = AsyncOpenAILLM(config)
+llm = AsyncOpenAILLM(base_config_for_llm)
 
 
 @router_v1.get("/", response_model=list[ConversationSchemas], status_code=status.HTTP_200_OK)
-async def get_conversations(
+async def get_conversation(
     current_user: UserModel = Depends(get_current_user),
     db: AsyncSession = Depends(get_async_postgres_db),
 ) -> list[ConversationModel]:
     """Получить все беседы пользователя"""
+    logger.info(f"Запрос на получение бесед пользователя {current_user.id}")
     result = await db.scalars(
         select(ConversationModel)
-        .where(ConversationModel.user_id == current_user.id, ConversationModel.is_archived.is_(False))
+        .where(ConversationModel.user_id == current_user.id)
         .order_by(ConversationModel.created_at.desc())
     )
 
@@ -64,11 +60,13 @@ async def create_conversation(
     db: AsyncSession = Depends(get_async_postgres_db),
 ) -> ConversationModel:
     """Создать новую беседу"""
-
+    logger.info(f"Запрос на создание беседы пользователем {current_user.id}")
     conversation = ConversationModel(user_id=current_user.id, title=conv_data.title or "New conversation")
     db.add(conversation)
     await db.commit()
     await db.refresh(conversation)
+
+    logger.info(f"Создана беседа {conversation.id} для пользователя {current_user.id}")
 
     return cast(ConversationModel, conversation)
 
@@ -79,69 +77,134 @@ async def get_messages(
     current_user: UserModel = Depends(get_current_user),
     limit: int = 50,
     db: AsyncSession = Depends(get_async_postgres_db),
-) -> list[MessageModel]:  # TODO: Добавить пагинацию
+) -> list[MessageModel]:
     """Получить историю сообщений отдельной беседы"""
+    logger.info(f"Запрос на получение беседы {conversation_id} пользователем {current_user.id}")
     await db.scalars(select(UserModel).where(UserModel.id == current_user.id))
 
     result = await db.scalars(
         select(MessageModel)
-        .where(MessageModel.conversation_id == conversation_id, ConversationModel.is_archived.is_(False))
-        .order_by(MessageModel.timestamp.desc())
+        .where(MessageModel.conversation_id == conversation_id)
+        .order_by(MessageModel.timestamp)
         .limit(limit)
     )
 
     messages = cast(list[MessageModel], result.all())
 
-    return list(reversed(messages))
+    return list(messages)
 
 
-@router_v1.post("/{conversation_id}/messages", status_code=status.HTTP_201_CREATED)
+@router_v1.post("/{conversation_id}/message", response_model=MessageSchemas, status_code=status.HTTP_201_CREATED)
 async def add_message(
     conversation_id: UUID,
     message: MessageCreate,
     background_tasks: BackgroundTasks,
     current_user: UserModel = Depends(get_current_user),
     db: AsyncSession = Depends(get_async_postgres_db),
-) -> dict:
+) -> MessageModel:
     """Добавить сообщение в беседу"""
-
+    logger.info(f"Запрос на добавление сообщения в беседу {conversation_id} пользователем {current_user.id}")
     # Проверка доступа
-    stmt = select(ConversationModel).where(
-        ConversationModel.id == conversation_id,
-        ConversationModel.user_id == current_user.id,
-        ConversationModel.is_archived.is_(False),
+    result = await db.scalars(
+        select(ConversationModel).where(
+            ConversationModel.id == conversation_id, ConversationModel.user_id == current_user.id
+        )
     )
-    result = await db.scalars(stmt)
+
     conversation = result.first()
 
     if not conversation:
-        raise HTTPException(status_code=404, detail="Беседа не найдена")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found")
 
     # Сохраняем сообщение пользователя
     user_message = MessageModel(
-        conversation_id=conversation_id, role=message.role, content=message.content, model=os.getenv("MODEL")
+        conversation_id=conversation_id, role=message.role, content=message.content, model=llm.config.model
     )
     db.add(user_message)
 
-    history = await get_conversation_history(db, conversation_id)
+    # Получаем историю беседы
+    history = await get_conversation_history(db, conversation_id, limit=10)
 
+    # Отправляем запрос к llm
     assistant_response = await llm.generate_response(history)
 
     # Сохраняем ответ ассистента
     assistant_message = MessageModel(
-        conversation_id=conversation_id, role="assistant", content=assistant_response, model=os.getenv("MODEL")
+        conversation_id=conversation_id, role="assistant", content=assistant_response, model=llm.config.model
     )
     db.add(assistant_message)
 
     await db.commit()
 
-    messages = [{"role": "assistant", "content": user_message.content}, {"role": "user", "content": assistant_response}]
-
+    # Передаём сообщение пользователя в mem0ai
     background_tasks.add_task(
-        memory_local.add, messages, user_id=current_user.username, run_id=str(conversation.id)
-    )  # TODO: Не работает должным образом выдаёт ошибки типизации у mem0ai
+        memory_local.add, messages=[message.model_dump()], user_id=current_user.username, run_id=str(user_message.id)
+    )
 
-    return {"user_message": user_message.content, "assistant_message": assistant_response}
+    logger.info(f"Сообщение добавлено в беседу {conversation_id}")
+
+    return assistant_message
+
+
+@router_v1.post("/{conversation_id}/message/stream", status_code=status.HTTP_200_OK)
+async def add_message_stream(
+    conversation_id: UUID,
+    message: MessageCreate,
+    background_tasks: BackgroundTasks,
+    current_user: UserModel = Depends(get_current_user),
+    db: AsyncSession = Depends(get_async_postgres_db),
+) -> StreamingResponse:
+    """
+    Добавить сообщение в беседу и получить стриминговый
+    """
+    logger.info(f"Запрос на добавления стримингового ответа в беседу {conversation_id} пользователем {current_user.id}")
+    # Проверка доступа и что беседа существует
+    result = await db.scalars(
+        select(ConversationModel).where(
+            ConversationModel.id == conversation_id,
+            ConversationModel.user_id == current_user.id,
+            ConversationModel.is_archived.is_(False),
+        )
+    )
+
+    conversation = cast(ConversationModel, result.first())
+
+    if not conversation:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found")
+
+    # Сохраняем сообщение пользователя
+    user_message = MessageModel(
+        conversation_id=conversation_id, role=message.role, content=message.content, model=llm.config.model
+    )
+    db.add(user_message)
+    await db.commit()
+
+    # Передаём сообщение на извлечение фактов в mem0ai
+    background_tasks.add_task(
+        memory_local.add,
+        messages=[message.model_dump()],
+        user_id=current_user.username,
+        run_id=str(user_message.id),
+    )
+
+    # Получаем историю с системным промтом и релевантными фактами для контекста
+    history = await get_conversation_history_with_mem0(
+        message=message.content,
+        user_name=current_user.username,
+        prompt=START_PROMPT,
+        db=db,
+        conversation_id=conversation_id,
+        limit=10,
+    )
+
+    # Передаём историю для генерации ответа
+    stream, result_awaitable = await llm.generate_stream_response(messages=history)
+
+    background_tasks.add_task(save_message_to_db_after_stream, result_awaitable, db, conversation_id, llm.config.model)
+
+    logger.info(f"Сообщение добавлено в беседу {conversation_id}")
+    # Возвращаем streming ответ
+    return StreamingResponse(stream, media_type="text/event-stream")
 
 
 @router_v1.delete("/{conversation_id}", status_code=status.HTTP_200_OK)
@@ -151,15 +214,17 @@ async def delete_conversation(
     db: AsyncSession = Depends(get_async_postgres_db),
 ) -> dict:
     """
-    Выполняет мягкое удаление беседы по UUID
+    Полное удаление беседы по UUID из БД (Включая сообщения)
     """
-
+    logger.info(f"Запрос на полное удаление беседы {conversation_id} пользователя {current_user.id}")
     conversation = await db.get(ConversationModel, conversation_id)
 
     if not conversation or conversation.user_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Conversation not found")
 
-    conversation.is_archived = True
+    await db.delete(conversation)
     await db.commit()
+
+    logger.info(f"Удалён беседа {conversation_id}")
 
     return {"message": "Conversation deleted"}
