8a61ec00f3822142b9e88524123a5e17a457507d
MaxSummerX <marspowerpops@gmail.com>
2026-01-17 20:56:14 +0300

    The endpoint for the test frontend has been removed.

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
-from pathlib import Path
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 
 from app.api.v1 import conversation, fact, prompts, tools, upload, users
+from app.api.v2 import conversation as conversation_v2
 from app.api.v2 import upload as upload_v2
 from app.api.v2 import users as users_v2
 from app.configs.settings import settings
@@ -13,10 +10,6 @@ from app.middleware.logging import log_middleware
 from app.middleware.security_middleware import add_security_headers
 from app.middleware.timing_middleware import TimingMiddleware
 
-
-# Статические файлы
-BASE_DIR = Path(__file__).resolve().parent
-STATIC_DIR = BASE_DIR / "static"
 
 # Создаём приложение FastAPI
 app = FastAPI(title="AI chat", version="0.1.1")
@@ -41,16 +34,6 @@ app.include_router(upload.router_v1, prefix="/api/v1")
 app.include_router(tools.router_V1, prefix="/api/v1")
 
 # Подключаем маршруты категорий 2ой версии
-app.include_router(upload_v2.router_v2, prefix="/api/v2")
-app.include_router(users_v2.router_v2, prefix="/api/v2")
-
-
-# Главная страница
-@app.get("/", include_in_schema=False)
-async def chat() -> FileResponse:
-    return FileResponse(STATIC_DIR / "chat.html")
-
-
-# Статические ресурсы
-if STATIC_DIR.exists():
-    app.mount("/", StaticFiles(directory=str(STATIC_DIR)), name="static")
+app.include_router(upload_v2.router, prefix="/api/v2")
+app.include_router(users_v2.router, prefix="/api/v2")
+app.include_router(conversation_v2.router, prefix="/api/v2")
