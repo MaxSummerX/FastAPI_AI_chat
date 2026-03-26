38fd0940fbd8ec13e955b06092d81766f5fa6e35
MaxSummerX <marspowerpops@gmail.com>
2026-03-26 21:02:28 +0300

    refactor(security): remove old app/auth after migration to infrastructure

--- app/auth/jwt_config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-
-from app.utils.env import get_required_env
-
-
-load_dotenv()
-
-SECRET_KEY = get_required_env("SECRET_KEY")
-ALGORITHM = get_required_env("ALGORITHM")
