e817bf205b6ceebbabe4d6e43d7a6b1d68463944
MaxSummerX <marspowerpops@gmail.com>
2026-03-27 13:11:55 +0300

    fix(admin): update router prefix to /api/admin

--- app/presentation/routers/admin/__init__.py
+++ app/presentation/routers/admin/__init__.py
@@ -4,7 +4,7 @@ from app.presentation.dependencies import get_current_admin_user
 from app.presentation.routers.admin import experiment, invite, role, statistics
 
 
-router = APIRouter(prefix="/admin", tags=["Admin"], dependencies=[Depends(get_current_admin_user)])
+router = APIRouter(prefix="/api/admin", tags=["Admin"], dependencies=[Depends(get_current_admin_user)])
 
 router.include_router(invite.router)
 router.include_router(statistics.router)
